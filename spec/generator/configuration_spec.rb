d7eaed18f07b331ca99366098f732b4c578280d6
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-18 16:58:00 +0200

    Specs -> refactored to make structure cleaner, added triangulation test where needed

--- spec/generator/configuration_spec.rb
+++ spec/generator/configuration_spec.rb
@@ -1,9 +1,27 @@
+################################################################################
+#                                                                              # 
+# Test cases                                                                   # 
+#                                                                              # 
+################################################################################
+
 describe Configuration do
   it 'contains a series of fragments' do
     # Initialize the configuration object
     fragments = {
       'base' => Fragment.new('base', ['php/base.php'], ['KEY']),
       'identify-alternatives' => Fragment.new('identify-alternatives', ['php/identify_alternatives.php'], [])
+    }
+    configuration = Configuration.new(fragments) 
+
+    # Expect the fragments to be accessible
+    expect(configuration.fragments).to eq(fragments)
+  end
+
+  it 'contains a different series of fragments' do
+    # Initialize the configuration object
+    fragments = {
+      'footer' => Fragment.new('footer', ['php/footer.php', 'php/footer_alt.php'], []),
+      'download-file' => Fragment.new('download-file', ['php/download-file'], ['SRC'])
     }
     configuration = Configuration.new(fragments) 
 
--- spec/generator/fragment_spec.rb
+++ spec/generator/fragment_spec.rb
@@ -1,7 +1,22 @@
+################################################################################
+#                                                                              # 
+# Test cases                                                                   # 
+#                                                                              # 
+################################################################################
+
 describe Fragment do
   it 'has a key that identifies it' do
     # Create a new fragment
     key = 'test-key'
+    fragment = Fragment.new(key, [], [])
+
+    # Expect the key to be stored and readable
+    expect(fragment.key).to eq(key)
+  end
+
+  it 'has a different key that identifies it' do
+    # Create a new fragment
+    key = 'different-test-key'
     fragment = Fragment.new(key, [], [])
 
     # Expect the key to be stored and readable
@@ -17,9 +32,27 @@ describe Fragment do
     expect(fragment.sources).to eq(sources)
   end
 
+  it 'stores different source files that must be included' do
+    # Create a new fragment
+    sources = ['php/base.php']
+    fragment = Fragment.new('base', sources, [])
+
+    # Expect the key to be stored and readable
+    expect(fragment.sources).to eq(sources)
+  end
+
   it 'stores a list of arguments required to correctly use the fragment' do
     # Create a new fragment
     arguments = ['KEY']
+    fragment = Fragment.new('setup-encryption', [], arguments)
+
+    # Expect the key to be stored and readable
+    expect(fragment.arguments).to eq(arguments)
+  end
+
+  it 'stores an empty list if there are no arguments required' do
+    # Create a new fragment
+    arguments = []
     fragment = Fragment.new('setup-encryption', [], arguments)
 
     # Expect the key to be stored and readable
