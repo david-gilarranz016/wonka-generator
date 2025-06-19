a5ba763acfe1638396088b2bd204371a799a4263
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-19 21:27:23 +0200

    Output -> has info for PNG file

--- lib/generator/output.rb
+++ lib/generator/output.rb
@@ -1,4 +1,10 @@
 module Output
   PHP = FileInfo.new('php')
   GIF = FileInfo.new('gif', '474946')
+  PNG = FileInfo.new(
+    'png',
+    '89504e470d0a1a0a0000000d494844520000000100000001010000000037'\
+    '6ef9240000000a4944415408d76368000000820081dd436af40000000049'\
+    '454e44ae426082'
+  )
 end
--- spec/generator/output_spec.rb
+++ spec/generator/output_spec.rb
@@ -24,4 +24,18 @@ describe Output do
     file_info = Output::GIF
     expect(file_info.preamble).to eq('474946')
   end
+
+  it 'stores the correct extension for a PNG file' do
+    file_info = Output::PNG
+    expect(file_info.extension).to eq('png')
+  end
+
+  it 'stores the correct preamble for a PNG file' do
+    file_info = Output::PNG
+    expect(file_info.preamble).to eq(
+      '89504e470d0a1a0a0000000d494844520000000100000001010000000037'\
+      '6ef9240000000a4944415408d76368000000820081dd436af40000000049'\
+      '454e44ae426082'
+    )
+  end
 end
